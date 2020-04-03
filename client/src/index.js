6875e77f082ac3453a38f76f49c62684731f4f71
Sami Karn <samkarn@publicisgroupe.net>
2020-04-04 02:33:23 +0530

    sass issues fixed

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { ApolloProvider as ApolloHooksProvider } from "@apollo/react-hooks";
 import initApolloClient from "./apollo/initApolloClient";
 //import { StoreProvider } from "./store";
 import App from "./components/app/App";
-
+import "./styles/index.scss";
 console.log("process env", process.env);
 console.log("server url", process.env.SERVER_URL);
 const SERVER_URL = "http://localhost:4000/graphql";
